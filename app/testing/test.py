5d48c0ba513e8e41f87b27a30d353de3d739c5eb
PuspaAyuAinurRahma <puspaayuainurrahma62@gmail.com>
2023-12-17 11:59:28 +0700

    Print debug

--- app/testing/test.py
+++ app/testing/test.py
@@ -18,5 +18,6 @@ def get_title_from_web(url):
 web_url = "http://127.0.0.1:5000"
 title = get_title_from_web(web_url)
 
+print("Title:", title)
 assert title == "Perpustakaan"
 print("Testing success!")
